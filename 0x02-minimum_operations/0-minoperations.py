0646f8910796ae01710feb3c58ef8b8db01535da
Victory Chibuike <victorychibuike121@gmail.com>
2024-10-17 21:11:22 +0100

    Added interpreter path

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,34 +8,22 @@ operations needed to result in exactly n H characters
 in the file.
 """
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n <= 1:
-        return False
-    if n == 2:
-        return True  # 2 is the only even prime number
-    if n % 2 == 0:
-        return False  # Eliminate even numbers greater than 2
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-
 
 def minOperations(n):
-    """
-    Method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
-    """
-    if n <= 1:
-        return 0
-    
+    """Calculates and Returns the fewest number of operations"""
     operations = 0
-    
-    while is_prime(n):
-        n = n - 1
-        operations += 1
-    
-    for i in range(2, n + 1):
-        if n % i == 0:
-            return minOperations(n // i) + i
-    return n + operations
+    curr = 1
+    copy = 0
+
+    if type(n) != int:
+        return operations
+
+    while curr < n:
+        if n % curr == 0:
+            copy = curr
+            curr += copy
+            operations += 2
+        else:
+            curr += copy
+            operations += 1
+    return operations
