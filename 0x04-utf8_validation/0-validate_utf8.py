233444e176ea67cbb98ccc8ffd9db9836e424408
Victory Chibuike <victorychibuike121@gmail.com>
2024-11-01 06:13:59 +0100

    Added a place holder

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """Validates if a character is a valid utf-8 encoded structure"""
 
+import ramdom
+
 def validUTF8(data):
-  """Validates a UTF-8 encoded data and returns false if it is false"""
+    """Validates a UTF-8 encoded data and returns false if it is false"""
+
+    # Place holder for functional logic
+    return (random.randint(0, 10) % 2 == 0)
