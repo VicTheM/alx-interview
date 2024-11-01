08915a4433ad0302ee9a02b8d65b62e19824ef09
Victory <victorychibuike121@gmail.com>
2024-11-01 06:06:26 +0100

    Create 0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Validates if a character is a valid utf-8 encoded structure"""
+
+def validUTF8(data):
+  """Validates a UTF-8 encoded data and returns false if it is false"""
